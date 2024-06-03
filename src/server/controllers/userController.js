98372a45f4da843f9e19cd7ad8f650ca6af4fc6a
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-03 14:29:02 +0800

    Encrypted password with bcryptjs

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,6 +1,7 @@
 import expressAsyncHandler from "express-async-handler";
 import { body, validationResult } from "express-validator";
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 const get_users = expressAsyncHandler(async (req, res) => {
   const users = await User.find().exec();
@@ -22,18 +23,24 @@ export const post_signup = [
       return value === req.body.password;
     }),
   expressAsyncHandler(async (req, res) => {
-    const user = new User({
-      username: req.body.username,
-      // encrypt
-      password: req.body.password
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        // create err middleware in app.js?
+        return next(err);
+      } else {
+        const user = new User({
+          username: req.body.username,
+          password: hashedPassword
+        });
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.redirect("/form");
+          return;
+        }
+        await user.save();
+        res.redirect("/");
+      }
     });
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.redirect("/form");
-      return;
-    }
-    await user.save();
-    res.redirect("/");
   })
 ];
 
