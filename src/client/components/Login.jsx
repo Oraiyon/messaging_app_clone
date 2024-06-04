e22da5a743e3ca16781120d55a2b75e8ec7a7f26
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-04 11:03:22 +0800

    Created link to /signup from /login

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,12 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "../stylesheets/login.module.css";
+
 const Login = () => {
   return (
-    <form method="post">
-      <label htmlFor="username">Username:</label>
-      <input type="text" name="username" id="username" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" name="password" id="password" />
-      <button>Login</button>
-    </form>
+    <>
+      <form method="post">
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <button>Login</button>
+      </form>
+      <div className={styles.signup}>
+        <p>Don't have an account yet?</p>
+        <Link to={"/signup"}>Sign Up!</Link>
+      </div>
+    </>
   );
 };
 
