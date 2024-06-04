90151eb6bfa2d0fce2865647157d2f8da136c065
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-04 10:49:23 +0800

    Added button to /signup

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -5,11 +5,18 @@ const Header = () => {
   return (
     <header>
       <h1>Hello World</h1>
-      <button>
-        <Link to={"/login"} className={styles.link}>
-          Login
-        </Link>
-      </button>
+      <div className={styles.header_buttons}>
+        <button>
+          <Link to={"/signup"} className={styles.link}>
+            Sign Up
+          </Link>
+        </button>
+        <button>
+          <Link to={"/login"} className={styles.link}>
+            Login
+          </Link>
+        </button>
+      </div>
     </header>
   );
 };
