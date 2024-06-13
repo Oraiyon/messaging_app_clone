532f4176b9da72b5d7bde024a845752409d6ea72
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-13 14:12:13 +0800

    Added input to be used for friend search

--- src/client/components/Messsages.jsx
+++ src/client/components/Messsages.jsx
@@ -6,9 +6,19 @@ const Messages = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.friends_list}>
-        {user.friends.length ? <h1>HELLO WORLD</h1> : <p>No friends...</p>}
+      <div className={styles.friends}>
+        <div className={styles.friends_list}>
+          {user.friends.length ? <h1>HELLO WORLD</h1> : <p>No friends...</p>}
+        </div>
+        <label htmlFor="friend_search"></label>
+        <input
+          type="text"
+          name="friend_search"
+          className={styles.friend_search}
+          id="friend_search"
+        />
       </div>
+
       <div className={styles.messages}>
         <form action="" method="post">
           <label htmlFor="message"></label>
