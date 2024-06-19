4e1a3869f8b1148462bdd5caba745fefc112a2fc
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-19 14:21:59 +0800

    Worked on friend request remove/ decline function

--- src/client/components/FriendRequestsModal.jsx
+++ src/client/components/FriendRequestsModal.jsx
@@ -1,11 +1,22 @@
 import styles from "../stylesheets/friendRequestsModal.module.css";
 
 const FriendRequestsModal = (props) => {
+  const removeFriendRequest = (index) => {
+    console.log("HI");
+  };
+
+  // Separate friend requests for sent or received?
   // Use user._id for key?
   return (
     <div className={styles.friend_request_modal + " friend_requests"}>
+      <h3>Friend Requests</h3>
       {props.user.friendRequests.map((request, index) => (
-        <p key={index}>{request.receiver}</p>
+        <div key={index} className={styles.friend_request}>
+          <p>{request.receiver === props.user.username ? request.sender : request.receiver}</p>
+          <button onClick={() => removeFriendRequest(index)}>
+            {request.receiver === props.user.username ? "Decline" : "Remove"}
+          </button>
+        </div>
       ))}
     </div>
   );
