311030bd8eb30e3ab9335333cd8ef89f962f73bc
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-06-26 12:34:04 +0800

    Update

--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   // ref = model
